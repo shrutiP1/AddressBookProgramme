9a6dee97deb63a4117f4e712f11601c1e681750e
shrutiP1 <shrutipatil13798@gmail.com>
2021-05-10 00:38:20 +0530

    Add:Ability to Add New Contact In Address Book

--- src/com/company/bridegelabz/adressbook/AddressBookMain.java
+++ src/com/company/bridegelabz/adressbook/AddressBookMain.java
@@ -1,10 +1,11 @@
 package com.company.bridegelabz.adressbook;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class AddressBookMain {
     static ArrayList<ContactInfo> user = new ArrayList<ContactInfo>();
-    static int flag = 0;
+
 
     public static void addContact() {
         ContactInfo a1 = new ContactInfo();
@@ -13,10 +14,42 @@ public class AddressBookMain {
         System.out.println(user);
     }
 
+    public static void chooseOption() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("choose one option from below \n 1:add contact \n 2:edit contact \n 3:delete contact");
+        int option = scan.nextInt();
+        switch (option) {
+            case 1:
+                AddressBookMain.addContact();
+                System.out.println("If you want to continue then press 1 or press 2 if you want to quit");
+                int num = scan.nextInt();
+                if (num == 1) {
+                    chooseOption();
+                } else {
+                    break;
+                }
+                break;
+//            case 2:
+//                editContact();
+//                break;
+//            case 3:
+//                deleteContact();
+//                break;
+            default:
+                System.out.println("You choose Wrong Option please choose right option");
+                //Main.chooseOption();
+                break;
+        }
+
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Programme");
         //UC1
         AddressBookMain.addContact();
+        //UC2
+        AddressBookMain.chooseOption();
+
 
     }
 }
