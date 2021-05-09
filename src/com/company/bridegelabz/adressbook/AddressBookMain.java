6ee6591e260b6023212c1c5063c53cf805760d13
shrutiP1 <shrutipatil13798@gmail.com>
2021-05-09 23:55:00 +0530

    Add: Welcome Message.

---
+++ src/com/company/bridegelabz/adressbook/AddressBookMain.java
@@ -0,0 +1,9 @@
+package com.company.bridegelabz.adressbook;
+
+public class AddressBookMain {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to Address Book Programme");
+    }
+}
