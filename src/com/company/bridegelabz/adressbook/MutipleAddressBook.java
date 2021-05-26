072d08f41ece6ea5e74d6f62bc874090a552bc17
shrutiP1 <shrutipatil13798@gmail.com>
2021-05-26 11:57:19 +0530

    Add:Ensuring No Duplicate Address Book are present

---
+++ src/com/company/bridegelabz/adressbook/MutipleAddressBook.java
@@ -0,0 +1,94 @@
+package com.company.bridegelabz.adressbook;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class MutipleAddressBook {
+    List<ContactInfo> contacts = new ArrayList<>();
+    Scanner input = new Scanner(System.in);
+    int index;
+    String firstName;
+    String name;
+
+    public MutipleAddressBook(String name) {
+        this.name = name;
+    }
+
+    public boolean checkName() {
+        System.out.println("Enter First Name");
+        firstName = input.nextLine();
+        for (index = 0; index < contacts.size(); index++) {
+            if (firstName.equals(contacts.get(index).getFirstName())) {
+                System.out.println("Contact found");
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void addContact() {
+        if (!checkName()) {
+            System.out.print("Enter Last Name: ");
+            String lastName = input.nextLine();
+            System.out.print("Enter Address: ");
+            String address = input.nextLine();
+            System.out.print("Enter City: ");
+            String city = input.nextLine();
+            System.out.print("Enter State: ");
+            String state = input.nextLine();
+            System.out.print("Enter Pin Code: ");
+            String zip = input.nextLine();
+            System.out.print("Enter Phone Number: ");
+            String phone = input.nextLine();
+            System.out.print("Enter Email ID: ");
+            String email = input.nextLine();
+            contacts.add(new ContactInfo(firstName, lastName, address, city, state, zip, phone, email));
+        }
+    }
+
+    public void editContact() {
+        if (checkName()) {
+            System.out.print("Enter New Name: ");
+            contacts.get(index).setFirstName(input.nextLine());
+            System.out.print("Enter New Address: ");
+            contacts.get(index).setAddress(input.nextLine());
+            System.out.print("Enter New City: ");
+            contacts.get(index).setCity(input.nextLine());
+            System.out.print("Enter New State: ");
+            contacts.get(index).setState(input.nextLine());
+            System.out.print("Enter New Pin Code: ");
+            contacts.get(index).setZip(input.nextLine());
+            System.out.print("Enter New Phone Number: ");
+            contacts.get(index).setPhoneNumber(input.nextLine());
+            System.out.print("Enter New Email ID: ");
+            contacts.get(index).setEmailId(input.nextLine());
+            System.out.println("Successfully Edited The Contact");
+        } else {
+            System.out.println("Name not Found!!");
+        }
+    }
+
+    public void deleteContact() {
+        System.out.println("Enter the name of Contact you want to Delete");
+        for (index = 0; index < contacts.size(); index++) {
+            if (checkName()) {
+                contacts.remove(index);
+                System.out.println("successfully Deleted The Contact");
+            }
+        }
+    }
+
+    public void printContacts() {
+        System.out.println("Contact is Address Book are :");
+        if (contacts.isEmpty()) {
+            System.out.println("No Contact Found");
+        }
+        List<ContactInfo> sortedArray = contacts.stream().sorted(Comparator.comparing(ContactInfo::getFirstName)).collect(Collectors.toList());
+        System.out.println(sortedArray);
+    }
+
+
+}
